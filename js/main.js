dd7f69edc77781c2757de9e2ece4f8c7fe28adb9
Jamil <jamilpleandro@gmail.com>
2022-10-27 18:58:18 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const hamburguerIcon = document.querySelector(".nav__hamburguer");
+const navOverlay = document.querySelector(".nav__overlay");
+let currentDropdown = navOverlay;
+
+let listElements = document.querySelectorAll(".nav__parent");
+
+listElements.forEach((listElement) => {
+  listElement.addEventListener("click", () => {
+    listElement.classList.toggle("arrow");
+  });
+});
+
+// Agrega / cambia la clase asignada al dar click al icon menu.
+hamburguerIcon.addEventListener("click", () => {
+  hamburguerIcon.classList.toggle("nav__hamburguer--open");
+
+  navOverlay.classList.toggle("nav__overlay--show");
+});
+
+navOverlay.addEventListener("click", (e) => {
+  e.preventDefault();
+  const currentElement = e.target;
+
+  if (isActive(currentElement, "nav__parent")) {
+    const subMenu = currentElement.parentElement.children[1];
+
+    if (window.innerWidth < 768) {
+      let height = subMenu.clientHeight == 0 ? subMenu.scrollHeight : 0;
+
+      subMenu.style.height = `${height}px`;
+    } else {
+      if (!isActive(subMenu, "nav__inner--show")) {
+        closeDropdown(currentDropdown);
+      }
+      subMenu.classList.toggle("nav__inner--show");
+
+      currentDropdown = subMenu;
+    }
+  }
+});
+
+function isActive(elemnt, string) {
+  return elemnt.classList.value.includes(string);
+}
+
+function closeDropdown(currentDropdown) {
+  if (isActive(currentDropdown, "nav__inner--show")) {
+    currentDropdown.classList.remove("nav__inner--show");
+  }
+}
+
+window.addEventListener("resize", () => {
+  closeDropdown(currentDropdown);
+  if (window.innerWidth > 768) {
+    const navInners = document.querySelectorAll(".nav__inner");
+    navInners.forEach((navInner) => {
+      navInner.style.height = "";
+    });
+  }
+});
